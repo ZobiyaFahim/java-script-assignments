a4ac11698de5167e519c2e1a69a99f7fea33d942
Zobiay Fahim <zobiayfahim@gmail.com>
2025-09-08 15:55:57 +0500

    js 1 was completed

---
+++ JS assignment1/app.java
@@ -0,0 +1,60 @@
+
+
+// Question 1
+// Write a script to greet your website visitor using JS alert
+// box.
+
+// Answer:1
+alert("hello world!");
+
+// Question 2
+// Write a script to display following message on your web
+// page:
+
+// Answer:2
+alert("Error! Please enter a valid password.");
+
+// Question 3
+// Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+// Answer:3
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Question 4
+// 4. Write a script to display following messages in sequence:
+
+// Answer:4
+alert("Welcome to JS Land...");
+
+// Question 5
+// 5. Generate the following message through browser’s developer console:
+// Answer:5
+
+alert("Hello... I can run JS through my web browser's console");
+
+// Question 6
+// 6. Make use of alerts in your new/existing HTML & CSS
+// project.
+
+// Answer:6
+
+alert("hello this is a java scvript alert");
+
+// Question 7
+// 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+
+// ALERTS | JAVASCRIPT
+
+// Page 3 of 3
+
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+
+// Answer:7
+
+
