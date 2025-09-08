d8740318e6774a40f5c5b5d78b81a15650b422e9
Zobiay Fahim <zobiayfahim@gmail.com>
2025-09-08 15:58:59 +0500

    js 2assignment was completed

---
+++ JS assignment2/app.js
@@ -0,0 +1,74 @@
+// ..... second assaigment .....
+// Question 1:
+// Declare a variable called username.
+// answer:1
+
+var studentName = "Zobiya";
+console.log(studentName);
+alert(studentName);
+
+// Question 2:
+// Declare a variable called myName & assign to it a string that represents your Full Name.
+// answer:2 
+var myName = "Zobiya fahim";
+console.log(myName);
+alert(myName);  
+// Question 3:
+// Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// answer:3
+var message = "Hello World";
+console.log(message);
+alert(message);
+// Question 4:
+// Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// answer:4 
+var studentName = "Zobioya fahim";
+var studentAge = "15 years old";
+var studentCourse = "Certified web Development Application";
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+// Question 5:  
+// Write a script to display the following alert using one JS variable:
+// answer:5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+console.log(pizza);
+alert(pizza);
+// Question 6:
+// Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).
+    // Show the blow mentioned message in an alert box.(Hint: use
+    // string concatenation)
+// answer:6
+var email = "my email address is example@example.com";
+console.log(email);
+alert("My email address is " + email);
+// Questiomn:7
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+// answer:7
+var book = "A smarter way to learn JavaScript";
+console.log(book);
+alert("I am trying to learn from the book " + book);
+// Question 8:
+// Write a script to display this in browser through JS
+// answer:8
+document.write("Yah! I can write HTML content through JavaScript");
+// Question 9:
+// Store following string in a variable and show in alert and
+// browser through JS
+// answer:9
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+console.log(design);
+alert(design);
+document.write(design);
+// ..... second assaigment end .....
+
+
+
